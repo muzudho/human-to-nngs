57168e6bce21b702f8e226b01e305476d732374b
むずでょ <muzudho1@gmail.com>
2021-02-11 18:40:04 +0900

    Logger

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+
+	e "github.com/muzudho/human-to-nngs/entities"
 )
 
 func main() {
@@ -11,5 +13,25 @@ func main() {
 	flag.Parse()
 	fmt.Println(flag.Args())
 	fmt.Printf("entryConfPath=%s", *entryConfPath)
+
+	// グローバル変数の作成
+	e.G = *new(e.GlobalVariables)
+
+	// ロガーの作成。
+	e.G.Log = *e.NewLogger(
+		"output/trace.log",
+		"output/debug.log",
+		"output/info.log",
+		"output/notice.log",
+		"output/warn.log",
+		"output/error.log",
+		"output/fatal.log",
+		"output/print.log")
+
+	// チャッターの作成。 標準出力とロガーを一緒にしただけです。
+	e.G.Chat = *e.NewChatter(e.G.Log)
+
+	// 標準出力への表示と、ログへの書き込みを同時に行います。
+	// e.G.Chat.Trace("Author: %s\n", e.Author)
 
 }
